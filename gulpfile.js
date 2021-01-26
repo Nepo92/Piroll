75f142372b806a9ac04d714fb64b477d1548205e
Alexandr Eremeev <eremeev.a.eu@mail.ru>
2021-01-26 15:32:31 +0700

    Update Piroll

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const concat = require('gulp-concat');
 const webp = require('gulp-webp');
 const webpHTML = require('gulp-webp-html');
 const webpCSS = require('gulp-webp-css');
-const imageMin = require('gulp-imagemin');
+// const imagemin = require('gulp-imagemin');
 const compress = require('gulp-uglify-es').default;
 
 function cleanApp() {
@@ -38,7 +38,7 @@ function createFonts() {
 }
 
 function createCss() {
-  return gulp.src(['app/sass/**/*.+(sass|scss)', '!app/sass/**/_*.+(sass||scss)'])
+  return gulp.src(['app/sass/**/*.+(sass|scss)'])
     .pipe(sass({
       outputStyle: 'expanded',
     }))
@@ -60,7 +60,17 @@ function createCss() {
 
 function createImg() {
   return gulp.src(['app/img/**/*.+(svg||webp||png||jpg||jpeg||gif)'])
-    .pipe(imageMin({ progressive: true }))
+    // .pipe(imagemin([
+    //   imagemin.gifsicle({ interlaced: true }),
+    //   imagemin.mozjpeg({ quality: 75, progressive: true }),
+    //   imagemin.optipng({ optimizationLevel: 10 }),
+    //   imagemin.svgo({
+    //     plugins: [
+    //       { removeViewBox: true },
+    //       { cleanupIDs: false },
+    //     ],
+    //   }),
+    // ]))
     .pipe(gulp.dest('dist/img'))
     .pipe(webp())
     .pipe(gulp.dest('dist/img'))
@@ -79,18 +89,35 @@ function createJs() {
     .pipe(browserSync.reload({ stream: true }));
 }
 
+function createPHP() {
+  return gulp.src('app/**/*.php')
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
 function createSprite() {
   return gulp.src('app/sprite.svg')
     .pipe(gulp.dest('dist'))
     .pipe(browserSync.reload({ stream: true }));
 }
 
+function createFiles() {
+  return gulp.src('app/files')
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function createPhpMailer() {
+  return gulp.src('app/phpMailer/**')
+    .pipe(gulp.dest('dist/phpMailer/'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
 function spy() {
   gulp.watch(['app/**/*.html', '!app/**/_*.html'], gulp.parallel('html'));
-  gulp.watch(['app/sass/**/*.+(scss||sass)', '!app/sass/**/_*.+(sass||scss)'], gulp.parallel('sass'));
-  gulp.watch('app/img/**/*.+(svg||webp||png||jpg||jpeg||gif)', gulp.parallel('img'));
+  gulp.watch(['app/sass/**/*.+(scss||sass)'], gulp.parallel('sass'));
   gulp.watch(['app/js/**/*.js', '!app/js/_*.js'], gulp.parallel('js'));
-  gulp.watch(['app/fonts/**/*.+(ttf||woff||woff2)', '!app/fonts/_*.+(ttf||woff||woff2)'], gulp.parallel('fonts'));
+  gulp.watch('app/sprite.svg', gulp.parallel('spriteSVG'));
 }
 
 gulp.task('del', cleanApp);
@@ -107,7 +134,13 @@ gulp.task('img', createImg);
 
 gulp.task('js', createJs);
 
+gulp.task('php', createPHP);
+
 gulp.task('spriteSVG', createSprite);
+
+gulp.task('files', createFiles);
+
+gulp.task('phpMailer', createPhpMailer);
 
 gulp.task('watch', spy);
 
